529c2271cc6851178e955370631b41fec0dad038
Pedro Henrique <phmartins.developer@gmail.com>
2025-03-31 23:57:10 -0300

    Starting Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import * as S from './styles.jsx'
+
+function App() {
+  return (
+    <S.Container>
+      <p>Pomodoro Project</p>
+    </S.Container>
+  );
+}
+
+export default App;
---
+++ src/styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+  background-color: #0D0D0D;
+  width: 100vw;
+  height: 100vh;
+
+  color: white;
+`
