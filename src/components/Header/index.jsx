e916c7dd9aa68fd2dbd516e202ca2fe1bbd5cb75
euluiz-gustavo <euluizgustavo.dev@gmail.com>
2025-04-06 20:10:44 -0300

    iniciando header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react'
 import * as S from './styles'
+
 export const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+  console.log(isActive);
+
   return (
     <S.Container>
-      <p>Header Component</p>
+      <S.LogoContainer>
+        <S.Title>Pomodoro</S.Title>
+      </S.LogoContainer>
+
+
+      <S.ToggleContainer>
+        {isActive ? (
+          <S.LightModeIcon />
+        ) : (
+          <S.DarkModeIcon />
+        )}
+
+        <S.ButtonsheaderContainer isActive={isActive}>
+          <S.ButtonTheme
+            isActive={isActive}
+            onClick={() => setIsActive(!isActive)}
+          />
+        </S.ButtonsheaderContainer>
+      </S.ToggleContainer>
     </S.Container>
   )
 }
+
